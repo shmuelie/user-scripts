cb2f20569a889eaf6f7e73f49de8b095b0101af2
Shmueli Englard <shmueli.yosef@englard.net>
2021-06-23 10:57:20 -0400

    Now updates current position information correctly

--- src/PlayerFMPWA.user.js
+++ src/PlayerFMPWA.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        PlayerFM PWA
 // @namespace   net.englard.shmuelie
-// @version     1.1.1
+// @version     1.2.0
 // @description Enables PWA features in PlayerFM web.
 // @author      Shmuelie
 // @match       https://player.fm/*
@@ -31,13 +31,6 @@
                 ],
                 title: p.currentEpisode.title
             });
-            if (ms.setPositionState) {
-                ms.setPositionState({
-                    duration: p.currentEpisode.duration,
-                    position: this.player.getCurrentTime(),
-                    playbackRate: player.getSpeed()
-                });
-            }
         }
 
         function onPause() {
@@ -48,9 +41,20 @@
             ms.playbackState = "none";
         }
 
+        function onTimeupdate() {
+            if (ms.setPositionState) {
+                ms.setPositionState({
+                    duration: p.currentEpisode.duration,
+                    position: this.player.getCurrentTime(),
+                    playbackRate: player.getSpeed()
+                });
+            }
+        }
+
         p.listen("play", onPlay);
         p.listen("pause", onPause);
         p.listen("ended", onEnded);
+        p.listen("timeupdate", onTimeupdate);
 
         ms.setActionHandler("pause", function () {
             p.togglePlayback(false);
