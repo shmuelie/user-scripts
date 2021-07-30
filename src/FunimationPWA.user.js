eb0af3c204d3618a8b16743636058d4074f18d8f
Shmueli Englard <shmueli.yosef@englard.net>
2021-07-30 13:33:37 -0400

    Fixed match for Funimation PWA to include the www

--- src/FunimationPWA.user.js
+++ src/FunimationPWA.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name        Funimation PWA
 // @namespace   net.englard.shmuelie
-// @version     1.0.0
+// @version     1.0.1
 // @description Enables PWA features in Funimation web.
 // @author      Shmuelie
-// @match       https://funimation.com/player/*
+// @match       https://www.funimation.com/player/*
 // @grant       none
 // @website     https://github.com/shmuelie/user-scripts/blob/main/src/FunimationPWA.user.js
 // @updateURL   https://raw.githubusercontent.com/shmuelie/user-scripts/main/src/FunimationPWA.user.js
